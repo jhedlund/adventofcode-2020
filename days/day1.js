5f1a3bfa84b158dac3fecc3cdd5f3151e24b39af
jhedlund <jhedlund@gmail.com>
2020-12-01 14:07:49 -0500

    moved input to assets

--- days/day1.js
+++ days/day1.js
@@ -2,15 +2,24 @@
 
 // expense report
 
-export function expenseReport(inputArray) {
+export function expenseReport(inputArray, expectedResult) {
+  let answer = 0;
   inputArray.forEach(function(x, xindex, array) {
     inputArray.forEach(function(y, yindex, yarray) {
       if (xindex != yindex && xindex > yindex) {
         if (Number(x) + Number(y) == 2020) {
-          console.log("Found at (" + xindex + ", " + yindex + ")");
-          console.log(Number(x) * Number(y));
+          //console.log("Found at (" + xindex + ", " + yindex + ")");
+
+          answer = Number(x) * Number(y);
         }
       }
     });
   });
+  let status = "FAILURE (expected " + expectedResult + "): ";
+  if (answer == expectedResult) {
+    status = "SUCCESS: ";
+  }
+  console.log(status + answer);
 }
+
+function sumArray(startValue, startIx, inputArray, depth) {}
--- index.js
+++ index.js
@@ -2,30 +2,32 @@
 import "./style.css";
 import * as day1 from "./days/day1.js";
 
+console.log(mydata);
 // Write Javascript code!
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>AdventOfCode 2020</h1>`;
 
 let sampleInput = [1721, 979, 366, 299, 675, 1456];
 
-day1.expenseReport(sampleInput);
+day1.expenseReport(sampleInput, 514579);
 
 let day1array = [];
 
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
-    console.log(xhr.status);
+    //console.log(xhr.status);
     //console.log(xhr.responseURL);
     //console.log(xhr.responseText);
     day1array = xhr.responseText.split("\n");
-    day1.expenseReport(day1array);
+    day1.expenseReport(day1array, 982464);
   }
 };
 
 xhr.open(
   "GET",
-  "https://raw.githubusercontent.com/jhedlund/adventofcode-2020/master/inputs/day1.txt",
+  "./assets/day1.txt",
+  //"https://raw.githubusercontent.com/jhedlund/adventofcode-2020/master/inputs/day1.txt",
   true
 );
 xhr.send();
