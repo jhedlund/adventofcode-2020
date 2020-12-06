66be3f039cd9df5034e1273fb3e469f73e44a141
jhedlund <jhedlund@gmail.com>
2020-12-06 16:17:56 -0500

    day 7 setup prior to midnight

---
+++ days/day7.js
@@ -0,0 +1,13 @@
+/*
+ * Day 7
+ *
+ *
+ */
+
+import Day from "./day.js";
+
+export default class Day7 extends Day {
+  constructor() {
+    super(7, [[]], [[], []], [,]);
+  }
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import Day3 from "./days/day3.js";
 import Day4 from "./days/day4.js";
 import Day5 from "./days/day5.js";
 import Day6 from "./days/day6.js";
+import Day7 from "./days/day7.js";
 
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>AdventOfCode 2020</h1>`;
@@ -13,12 +14,13 @@ appDiv.innerHTML = `<h1>AdventOfCode 2020</h1>`;
 let dayix = 1;
 
 let days = [
-  new Day6(),
-  new Day5(),
-  new Day4(),
-  new Day3(),
+  new Day1(),
   new Day2(),
-  new Day1()
+  new Day3(),
+  new Day4(),
+  new Day5(),
+  new Day6(),
+  new Day7()
 ];
 
 function runDays() {
@@ -35,7 +37,7 @@ function done() {
   tbl += "<tr>";
   tbl += "<th>Day</th><th>Star</th><th></th><th>Result</th><th>Extra Data</th>";
   tbl += "</tr>";
-  days.forEach(function(day) {
+  days.reverse().forEach(function(day) {
     day.results.results.forEach(function(result) {
       tbl += "<tr class=' " + result.status + "'>";
       tbl += "<td>" + day.day + "</td>";
