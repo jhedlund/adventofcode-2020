7972b22ef379a7c517fee01340785cfc5a2f6533
jhedlund <jhedlund@gmail.com>
2020-12-06 15:52:33 -0500

    Day 5 star 2

--- days/day5.js
+++ days/day5.js
@@ -8,11 +8,12 @@ import Day from "./day.js";
 
 export default class Day5 extends Day {
   constructor() {
+    // Note:  Star2 did not have samples, so just setting expected to 0
     super(
       5,
       [["FBFBBFFRLR"], ["BFFFBBFRRR"], ["FFFBBBFRRR"], ["BBFFBBFRLL"]],
-      [[357, 567, 119, 820], []],
-      [994]
+      [[357, 567, 119, 820], [0, 0, 0, 0]],
+      [994, 741]
     );
   }
 
@@ -20,15 +21,46 @@ export default class Day5 extends Day {
     return this.maxSeatID(input);
   }
 
-  maxSeatID(input) {
+  star2(input) {
+    return this.maxSeatID(input, "myseat");
+  }
+
+  maxSeatID(input, fn = "highest") {
     let highest = 0;
+    let result = 0;
     let self = this;
+
+    let allSeats = [];
+
     input.forEach(function(boardingpass) {
       let x = self.seatID(boardingpass);
+
+      allSeats.push(x);
+
       if (x > highest) {
         highest = x;
       }
     });
+
+    // 609 was too low, 742 too high
+    if (fn == "myseat") {
+      allSeats = allSeats.sort(function(a, b) {
+        return b - a;
+      });
+
+      let sequence = highest;
+
+      for (let ix = 1; ix < allSeats.length; ix++) {
+        if (allSeats[ix] != sequence - 1) {
+          result = sequence - 1;
+          sequence = allSeats[ix];
+        } else {
+          sequence--;
+        }
+      }
+      return [result, ""];
+    }
+
     return [highest, ""];
   }
 
