a7a45b52270fffd3d081ed0efa1cb47dcdfffad5
jhedlund <jhedlund@gmail.com>
2020-12-14 10:19:50 -0500

    day 11 star 2

--- days/day11.js
+++ days/day11.js
@@ -26,19 +26,27 @@ export default class Day11 extends Day {
           "L.LLLLL.LL"
         ]
       ],
-      [[37], []],
-      [,]
+      [[37], [26]],
+      [2359, 2131]
     );
   }
 
   star1(input) {
+    return this.equalizeSeats(input);
+  }
+
+  star2(input) {
+    return this.equalizeSeats(input, 5, true);
+  }
+
+  equalizeSeats(input, occupiedMinimumRule = 4, firstVisible = false) {
     let layoutsEqual = false;
     let rounds = 0;
     let prevInput = input;
     let newInput = [];
     while (!layoutsEqual && rounds < 900) {
       rounds++;
-      newInput = this.seatRound(prevInput);
+      newInput = this.seatRound(prevInput, occupiedMinimumRule, firstVisible);
       layoutsEqual = equals(newInput, prevInput);
       prevInput = newInput;
     }
@@ -46,7 +54,7 @@ export default class Day11 extends Day {
     return [this.countSeats(newInput, "#"), ""];
   }
 
-  seatRound(input) {
+  seatRound(input, occupiedMinimumRule, firstVisible) {
     let result = [];
     let self = this;
     input.forEach(function(row, rowIx) {
@@ -54,12 +62,13 @@ export default class Day11 extends Day {
       for (let colIx = 0; colIx < row.length; colIx++) {
         if (
           row.charAt(colIx) == "L" &&
-          self.countAdjacent(input, rowIx, colIx, "#") == 0
+          self.countAdjacent(input, rowIx, colIx, "#", firstVisible) == 0
         ) {
           newRow += "#";
         } else if (
           row.charAt(colIx) == "#" &&
-          self.countAdjacent(input, rowIx, colIx, "#") >= 4
+          self.countAdjacent(input, rowIx, colIx, "#", firstVisible) >=
+            occupiedMinimumRule
         ) {
           newRow += "L";
         } else {
@@ -81,63 +90,75 @@ export default class Day11 extends Day {
     });
     return result;
   }
-  countAdjacent(input, row, col, seatState) {
+
+  countState(direction, input, row, col, seatState, recurse = false) {
+    let testcol = col;
+    let testrow = row;
+    switch (direction) {
+      case "nw":
+        testcol = col - 1;
+        testrow = row - 1;
+        break;
+      case "n":
+        testrow = row - 1;
+        break;
+      case "ne":
+        testrow = row - 1;
+        testcol = col + 1;
+        break;
+      case "e":
+        testcol = col + 1;
+        break;
+      case "se":
+        testcol = col + 1;
+        testrow = row + 1;
+        break;
+      case "s":
+        testrow = row + 1;
+        break;
+      case "sw":
+        testrow = row + 1;
+        testcol = col - 1;
+        break;
+      case "w":
+        testcol = col - 1;
+        break;
+    }
+    if (
+      testcol >= 0 &&
+      testrow >= 0 &&
+      testrow < input.length &&
+      testcol < input[testrow].length
+    ) {
+      let seat = input[testrow].charAt(testcol);
+      if (seat == seatState) {
+        return 1;
+      } else if (recurse && seat == ".") {
+        return this.countState(
+          direction,
+          input,
+          testrow,
+          testcol,
+          seatState,
+          recurse
+        );
+      }
+    }
+    return 0;
+  }
+
+  countAdjacent(input, row, col, seatState, recurse = false) {
     let result = 0;
 
     // top left
-    if (col > 0 && row > 0 && input[row - 1].charAt(col - 1) == seatState) {
-      result++;
-    }
-
-    //top
-    if (row > 0 && input[row - 1].charAt(col) == seatState) {
-      result++;
-    }
-
-    // top right
-    if (
-      row > 0 &&
-      col < input[row - 1].length - 1 &&
-      input[row - 1].charAt(col + 1) == seatState
-    ) {
-      result++;
-    }
-
-    // right
-    if (
-      col < input[row].length - 1 &&
-      input[row].charAt(col + 1) == seatState
-    ) {
-      result++;
-    }
-
-    // bottom right
-    if (
-      row < input.length - 1 &&
-      col < input[row + 1].length - 1 &&
-      input[row + 1].charAt(col + 1) == seatState
-    ) {
-      result++;
-    }
-
-    // bottom
-    if (row < input.length - 1 && input[row + 1].charAt(col) == seatState) {
-      result++;
-    }
-
-    //bottom left
-    if (
-      row < input.length - 1 &&
-      col > 0 &&
-      input[row + 1].charAt(col - 1) == seatState
-    ) {
-      result++;
-    }
-
-    // left
-    if (col > 0 && input[row].charAt(col - 1) == seatState) {
-      result++;
-    }
+    result += this.countState("nw", input, row, col, seatState, recurse);
+    result += this.countState("n", input, row, col, seatState, recurse);
+    result += this.countState("ne", input, row, col, seatState, recurse);
+    result += this.countState("e", input, row, col, seatState, recurse);
+    result += this.countState("se", input, row, col, seatState, recurse);
+    result += this.countState("s", input, row, col, seatState, recurse);
+    result += this.countState("sw", input, row, col, seatState, recurse);
+    result += this.countState("w", input, row, col, seatState, recurse);
 
     return result;
   }
